fc876b2ac30124578c3e67e4145ab98187f2a5ac
EduardoOliveira-S <contatoeduardo57@gmail.com>
2025-04-01 22:12:44 -0300

    Update myWorld.py

--- myWorld.py
+++ myWorld.py
@@ -1,4 +1,3 @@
 print ("Hello World")
-
 print("Hello World! And hello to my first bug, which I will fix... eventually.")
 print("Se você não entendeu a linha acima, é melhor estudar inglês também. 😆")
