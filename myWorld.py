f206230d4a1b2aaf6de3993b5320d2edffea8e23
Kennedy Bacelar <kennedybacelar@yahoo.com.br>
2025-04-01 21:42:41 -0300

    feat: greet the world and my first inevitable bug

--- myWorld.py
+++ myWorld.py
@@ -1,1 +1,4 @@
 print ("Hello World")
+
+print("Hello World! And hello to my first bug, which I will fix... eventually.")
+print("Se você não entendeu a linha acima, é melhor estudar inglês também. 😆")
